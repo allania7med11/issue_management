253ed7184886dce1716b3eb31de0b54d2f510869
allania7med11 <allania7med11@gmail.com>
2024-09-15 09:16:31 +0100

    add create and read endpoints for issues

--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import json
+from django.http import HttpRequest, JsonResponse
+from issues.models import Issue
 
-# Create your views here.
+issue_fields = ["id", "title", "description"]
+issue_required_fields = ["title", "description"]
+
+
+def issues(request: HttpRequest):
+    if request.method == "GET":
+        issues = Issue.objects.values(*issue_fields)
+        return JsonResponse({"data": list(issues)})
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        for field in issue_required_fields:
+            if not field in data or not data[field]:
+                return JsonResponse(
+                    {"message": f"{field} field is required"}, status=400
+                )
+        issue = Issue.objects.create(**data)
+        issues = Issue.objects.filter(id=issue.id).values(*issue_fields)
+        return JsonResponse({"data": list(issues)[0]})
